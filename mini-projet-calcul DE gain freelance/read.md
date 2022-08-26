63becd21e0a9f8abd817fbec07f046f649126011
Damour8333 <98494743+Damour8333@users.noreply.github.com>
2022-08-26 10:32:03 +0200

    correction du commit

--- mini-projet-calcul DE gain freelance/read.md
+++ mini-projet-calcul DE gain freelance/read.md
@@ -1,3 +1,10 @@
 38.5
+https://app.studi.fr/#/dashboard/events/30921/replay
+https://github.com/ThomasBDC/StudiLive2022ProjetJS
+//commit de l'episode 2
+https://github.com/ThomasBDC/StudiLive2022ProjetJS/commit/e79cc4b9caa49733faf2bb2cb6d1cb6beec342f2
+2.5 de travail le 22/08/22
 
-2.5 de travail
+
+3eme live
+je m'arréte 46 min
--- mini-projet-calcul DE gain freelance/script.js
+++ mini-projet-calcul DE gain freelance/script.js
@@ -1,26 +1,100 @@
+//TODO LIST : 
+//1 - Stocker en cookie le formulaire, pour le remplir avec notre dernier calcul
+//2 - Historique de calcul (seulement si clic sur calculer)
+//Imprimer la page ??
+//Convertir en pdf ou Excel ??
+//Animation ??
+//Envoyer mail ??
+
 function CalculGain(){
-    //on récupére le formulaire dans le html
-    let myForm = document.getElementById('formCalculGain');
-    //on le transforme en objet en forme data 
+    //On vérifie les inputs
+    CheckInputs();
+
+    //On récupère le formulaire dans le html    
+    let myForm = document.getElementById("formCalculGain");
+    //On le transforme en objet FormData
     let formObj = new FormData(myForm);
-// On récupére les inputs de notre formulaire par leurs noms
-    let tauxHoraire =  formObj.get('TH');
-    let tauxJournalier =  formObj.get('TJM');
-    let Extras =  formObj.get('Extras');
 
-    let qtetauxHoraire =  formObj.get('QteTH');
-    let qtetauxJournalier =  formObj.get('QteTJM');
-    let qteExtras =  formObj.get('QteExtras');
+    //On récupère les inputs de notre formulaire par leurs noms
+    let tauxHoraire = formObj.get('TH');
+    let tauxJournalier = formObj.get('TJM');
+    let extras = formObj.get('Extras');
+
+    let qtetauxHoraire = formObj.get('QteTH');
+    let qtetauxJournalier = formObj.get('QteTJM');
+    let qteextras = formObj.get('QteExtras');
+
     let charges = formObj.get('Charges');
-    //on commence le calcul
-    let gainHeure = tauxHoraire *  qtetauxHoraire;
-    //alert(gainHeure);
 
-    let gainJour =tauxJournalier *qtetauxJournalier;
+    //On commence le calcul
+    let gainHeure = tauxHoraire * qtetauxHoraire;
 
-    let gainExtras = Extras * qteExtras;
+    let gainJour = tauxJournalier * qtetauxJournalier;
 
-    let total = gainHeure + gainJour +gainExtras;
+    let gainExtras = extras * qteextras;
 
-    document.getElementById('resultatBrut').innerText=(total +"€");
+    let totalBrut = gainHeure + gainJour + gainExtras;
+
+
+    let chargeADeduire = (totalBrut * (charges/100));
+    let totalNet = totalBrut - chargeADeduire;
+
+    document.getElementById("resultatBrut").innerText = totalBrut.toFixed(2)+" €";
+    document.getElementById("resultatDifference").innerText = chargeADeduire.toFixed(2)+" €";
+    document.getElementById("resultatNet").innerText = totalNet.toFixed(2)+" €";
+
 }
+
+function CheckInputs(){
+    let mesInputs = document.querySelectorAll('#formCalculGain input.form-control');
+
+    mesInputs.forEach(monInput => {
+        //Vérifier si il vaut 0 ou plus
+        if(monInput.value < 0){
+            monInput.value = 0;
+        }
+
+        saveElementInCookies(monInput);
+    });
+}
+
+function saveElementInCookies(input){
+    document.cookie = input.name+'='+input.value;
+}
+
+
+function getCookie(input){
+    let mesCookies = document.cookie;
+
+    const name = input.name + '='; 
+    const tableauCookies = mesCookies.split('; ');
+    let valeurCookie = null;
+    tableauCookies.forEach(cookie =>{
+        if(cookie.indexOf(name) === 0){
+            //on a chopé le bon cookie
+            valeurCookie = cookie.substring(name.length);
+            console.log(valeurCookie);
+        }
+    });
+    return valeurCookie;
+}
+
+//Ajout des évènements
+let btn = document.getElementById("buttonValidation");
+btn.addEventListener('click', CalculGain);
+
+let mesInputs = document.querySelectorAll('#formCalculGain input.form-control');
+
+mesInputs.forEach(monInput => {
+    //Si il a une valeur en cookie, lui donner
+    let cookie = getCookie(monInput);
+
+    if(cookie != undefined && cookie != null){
+        monInput.value = cookie;
+    }
+
+
+    monInput.addEventListener('keyup', CalculGain);
+    monInput.addEventListener('change', CalculGain);
+});
+CalculGain();
